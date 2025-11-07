046bb6e1de85dcdd5646820f9569f9d8b9583c3f
Jason Gutierrez <jasongutierrez316@gmail.com>
2025-11-07 15:21:30 -0800

    Update src.c

--- PART2/src.c
+++ PART2/src.c
@@ -1,20 +1,4 @@
 /* USER CODE BEGIN Header */
-/**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -119,7 +103,7 @@ int main(void)
               HAL_UART_Transmit(&hlpuart1, (uint8_t*)msg, (uint16_t)n, 50);
           }
       }
-      HAL_Delay(200);
+      HAL_Delay(100);
   }
   /* USER CODE END 3 */
 }
